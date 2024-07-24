25ee1b9ce272301638be51cb2e67a43605f65db7
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2024-07-23 20:13:26 -0700

    Allowed Render to choose port

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -75,7 +75,7 @@ app.post('/api/notes', (request, response) => {
     response.json(notes)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
